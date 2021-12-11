2743ea33b4299879b6669ef634f151c6798dc93f
salma-n <64871295+salma-n@users.noreply.github.com>
2021-12-11 21:37:26 +0100

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -5,6 +5,15 @@ public class Client {
     public static void main(String[] args) {
         try {
            MyInterface service = (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server");
+           Scanner s = new Scanner(System.in);
+           String s1 = s.nextLine();
+           System.out.println(service.reverse(s1));
+           s1= s.nextLine();
+           System.out.println(service.minChar(s1));
+           s1= s.nextLine();
+           System.out.println(service.upperLower(s1));
+           scanner.close();
+            
 
         } catch (Exception e) {
             e.printStackTrace();
